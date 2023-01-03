35924dd5877190cf6b51a105efe6e85b42230ecd
DevWeb13 <giuliano.loic@sfr.fr>
2023-01-03 21:30:26 +0100

    feat: (App.js): Update App JS Component: Import and use Users.js components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Users from "./components/Users";
 function App() {
-  return <div>learn react</div>;
+  return (
+    <div className="container mt-5">
+      <h2>React Query Fetch State Management Example</h2>
+      <Users />
+    </div>
+  );
 }
 export default App;
